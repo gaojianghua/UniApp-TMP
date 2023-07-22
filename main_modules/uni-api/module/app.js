27cc71defe513d6ccf8c19d69fe2b373e8eac5da
高江华 <g598670138@163.com>
2023-07-22 15:45:41 +0800

    新增退出APP的api

--- main_modules/uni-api/module/app.js
+++ main_modules/uni-api/module/app.js
@@ -1,3 +1,11 @@
 export default {
-	
+	// 退出 APP
+	exitApp() {
+		let platform = uni.getSystemInfoSync().platform
+		if (platform == 'ios'){
+		    plus.ios.import("UIApplication").sharedApplication().performSelector("exit")
+		} else if (platform == 'android'){
+		    plus.runtime.quit();
+		}
+	}
 }
