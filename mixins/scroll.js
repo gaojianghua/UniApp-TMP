badb5b7e0f22359281ec632201e5e0319a117788
高江华 <g598670138@163.com>
2023-12-27 16:03:38 +0800

    优化

--- mixins/scroll.js
+++ mixins/scroll.js
@@ -2,16 +2,17 @@ export default {
 	data() {
 		return {
 			scrollTop: 0,
+			newScrollTop: 0
 		}
 	},
 	methods: {
 		// 滚动事件
 		scroll(i) {
-			this.scrollTop = i
+			this.newScrollTop = i
 		},
 		// 点击返回顶部
 		backTop() {
-			this.scrollTop = 0
+			this.scrollTop == 0 ? this.scrollTop = 0.1 : this.scrollTop = 0
 		}
 	}
 }
