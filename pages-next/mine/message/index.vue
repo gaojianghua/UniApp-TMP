ec087e2d2e23fc500832cb4fd206d2b6884ccdf0
高江华 <g598670138@163.com>
2024-02-19 15:11:49 +0800

    更新消息模块

--- pages-next/mine/message/index.vue
+++ pages-next/mine/message/index.vue
@@ -1,13 +1,50 @@
 <template>
 	<view class="page">
 		<m-navbar bgColor="#fff" textColor="#fb7299" value="page.消息中心" i18n></m-navbar>
+		<view class="content p-3">
+			<view class="card bg-white pl-3">
+				<view class="card-item pr-3 d-flex a-center" v-for="(item, i) in menus" :key="i"
+				@click="$tools.Navigate.navigateTo('/pages-offspring/message-list/index')">
+					<u-image width="60rpx" height="60rpx" :src="item.src"></u-image>
+					<view class="card-item-r w-100 d-flex a-center ml-2 py-2">
+						<view class="">
+							<view class="item-title">
+								{{$t(item.name)}}
+							</view>
+							<view class="item-msg font text-ellipsis1">
+								{{$t(item.message)}}
+							</view>
+						</view>
+						<u-icon class="ml-auto" name="arrow-right" size="16"></u-icon>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {}
+			return {
+				menus: [
+					{
+						name: '活动通知',
+						src: 'https://gongyue-shop.oss-cn-hangzhou.aliyuncs.com/img/common/alarm.svg',
+						message: '尊敬的用户：相亲活动已开发',
+					},
+					{
+						name: '系统通知',
+						src: 'https://gongyue-shop.oss-cn-hangzhou.aliyuncs.com/img/common/alarm.svg',
+						message: '尊敬的用户：小说及漫画等娱乐功能已上线',
+					},
+					{
+						name: '服务通知',
+						src: 'https://gongyue-shop.oss-cn-hangzhou.aliyuncs.com/img/common/alarm.svg',
+						message: '尊敬的用户：您提交的反馈已受理，我们会尽快为您解决问题',
+					}
+				]
+			}
 		},
 		methods: {
 
@@ -16,4 +53,20 @@
 </script>
 
 <style lang="scss" scoped>
+	.page{
+		.content{
+			.card{
+				.card-item{
+					.card-item-r{
+						border-bottom: 2rpx solid #cdcdcd;
+					}
+				}
+				.card-item:last-child{
+					.card-item-r{
+						border-bottom: none;
+					}
+				}
+			}
+		}
+	}
 </style>
--- pages-offspring/message-list/index.vue
+++ pages-offspring/message-list/index.vue
@@ -1,16 +1,96 @@
 <template>
 	<view class="page">
 		<m-navbar bgColor="#fff" textColor="#fb7299" value="page.消息列表" i18n></m-navbar>
+		<!-- 内容区域 -->
+		<m-scroll-y :isLoading="isLoading" i18n :scrollStyle="scrollStyle" :load="load" @loadmore="loadmore" bgColor="transparent"
+			@onRefresh="onRefresh">
+			<u-empty v-if="load != 0 && list.length == 0" mode="list" :text="$t('暂无数据')"
+				icon="http://cdn.uviewui.com/uview/empty/list.png">
+			</u-empty>
+			<view class="px-2">
+				<view v-if="list.length != 0" class="item bg-white rounded-1 d-flex a-center j-sb p-2 mt-2"
+					v-for="(item, i) in list" :key="i" @click="$tools.Navigate.navigateTo('/pages-offspring/message-detail/index', item.id)">
+					
+				</view>
+			</view>
+		</m-scroll-y>
 	</view>
 </template>
 
 <script>
+	import res from './data.json'
 	export default {
 		data() {
-			return {}
+			return {
+				load: 0,
+				isLoading: true,
+				query: {
+					page: 1,
+					limit: 10
+				},
+				list: [],
+				total: 0
+			}
+		},
+		onLoad() {
+			this.init()
 		},
 		methods: {
-
+			// 初始化
+			init() {
+				this.getData()
+			},
+			// 获取数据
+			async getData(e) {
+				// let {
+				// 	data,
+				// 	code,
+				// 	message
+				// } = await prayData(this.query)
+				let code = 200
+				let data = {
+					list: []
+				}
+				data.list = res.data
+				data.total = data.list.length
+				if (code == 200) {
+					if (e) { // 加载更多
+						this.list = this.list.concat(data.list)
+					} else {
+						this.list = data.list
+						this.total = data.total
+					}
+					if (this.query.page * this.query.limit >= this.total) {
+						return this.load = 1
+					} else {
+						return this.load = 2
+					}
+				}
+			},
+			// 下拉刷新
+			onRefresh() {
+				this.query.page = 1
+				this.getData()
+			},
+			// 加载更多
+			loadmore() {
+				if (this.load == 1 || this.istrig == false) return;
+				this.load = 0
+				this.query.page++
+				this.istrig = false
+				let time = setTimeout(() => {
+					this.getData('S')
+					this.istrig = true
+					clearTimeout(time)
+				}, 1000)
+			},
+		},
+		computed: {
+			scrollStyle() {
+				return {
+					height: `calc(100vh - ${this.$store.state.browserFitHeight}px - ${this.$store.state.navbarHeight}px - env(safe-area-inset-bottom) - ${this.$store.state.statusHeight}px)`
+				}
+			}
 		}
 	}
 </script>
