764fd5504d26431d70dba2ea62718e2593173aaf
高江华 <g598670138@163.com>
2024-02-25 23:12:19 +0800

    更新

--- pages-offspring/message-detail/index.vue
+++ pages-offspring/message-detail/index.vue
@@ -1,13 +1,19 @@
 <template>
 	<view class="page">
 		<m-navbar bgColor="#fff" textColor="#fb7299" value="page.消息详情" i18n></m-navbar>
+		<!-- 内容区域 -->
+		<m-scroll-y :isLoading="false" :isCustomRefresh="false" >
+			
+		</m-scroll-y>
 	</view>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {}
+			return {
+				
+			}
 		},
 		methods: {
 
