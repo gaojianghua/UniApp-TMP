134255e1612676f073b8e0ced49b40e6760c43da
高江华 <g598670138@163.com>
2024-03-02 16:06:10 +0800

    新增H5的cookie操作

--- main_modules/uni-api/module/h5.js
+++ main_modules/uni-api/module/h5.js
@@ -13,19 +13,69 @@ export default {
 		let str = ''
 		Object.keys(argsObj).forEach((item, i) => {
 			if (i == 0) {
-				str += `${item}${eleChar}${argsObj[item]}` 
-			}else {
-				str += `${keyValueCahr}${item}${eleChar}${argsObj[item]}` 
+				str += `${item}${eleChar}${argsObj[item]}`
+			} else {
+				str += `${keyValueCahr}${item}${eleChar}${argsObj[item]}`
 			}
 		})
 		location.href = `${schema}://${str}`
 		let time = setTimeout(() => {
 			if (store.state.appSystemInfo.platform == 'ios') {
 				location.href = `https://apps.apple.com/app/${$store.state.iosAppName}`
-			}else if (store.state.appSystemInfo.platform == 'android') {
+			} else if (store.state.appSystemInfo.platform == 'android') {
 				location.href = `https://play.google.com/store/apps/details?id=${$store.state.androidAppName}`
 			}
 			clearTimeout(time)
 		}, 300)
+	},
+	/**
+	 * @description H5获取Cookie
+	 * @param name(String) 名称
+	 */
+	getCookie(name) {
+		let reg = new RegExp("(^| )" + name + "(?:=([^;]*))?(;|$)")
+		let val = document.cookie.match(reg)
+		return val ? (val[2] ? unescape(val[2]) : "") : null;
+	},
+	/**
+	 * @description H5设置Cookie
+	 * @param name(String) 名称
+	 * @param value(String) 值
+	 * @param pexpire(Number) 过期时间
+	 * @param ppath(String) 存储路径
+	 * @param pdomain(String) 域名IP
+	 * @param psecure(String) 安全
+	 */
+	setCookie(name, value, pexpire, ppath, pdomain, psecure) {
+		let exp = new Date()
+		let expires = arguments[2] || null
+		let path = arguments[3] || "/"
+		let domain = arguments [4] || null
+		let secure = arguments[5] || false
+		expires ? exp.setMinutes(exp.getMinutes() + parseInt(expires)) : "";
+		document.cookie = name + '=' + escape(value)
+		+ (expires ? ';expires=' + exp.toGMTString() : '') + (path ? ';path=' + path : '') + (domain ?
+		';domain=' + domain :
+		'') + (secure ? ';secure' :
+		'')
+	},
+	/**
+	 * @description H5删除Cookie
+	 * @param name(String) 名称
+	 * @param path(String) 存储路径
+	 * @param domain(String) 域名IP
+	 * @param secure(String) 安全
+	 */
+	removeCookie(name, path, domain, secure) {
+		let value = this.getCookie(name);
+		if (value !== null) {
+			let exp = new Date();
+			exp.setMinutes(exp.getMinutes() - 1000);
+			path = path || "/";
+			document.cookie = name +
+				'=;expires=' + exp.toGMTString() + (path ? ';path=' + path : '') + (domain ? ';domain=' + domain : '') +
+				(secure ?
+					';secure' : '')
+		}
 	}
 }
