2f20c611044c346a43c47bfc13ca6b8d735e3bfc
高江华 <g598670138@163.com>
2024-08-06 15:38:18 +0800

    更新语言配置

--- locale/index.js
+++ locale/index.js
@@ -1,8 +1,10 @@
 import en from './en.json'
 import zhHans from './zh-Hans.json'
 import zhHant from './zh-Hant.json'
+import th from './th.json'
 export default {
 	'zh-Hans': zhHans,
 	'zh-Hant': zhHant,
-	'en': en
+	'en': en,
+	'th': th
 }
