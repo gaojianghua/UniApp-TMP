978791454204881c6a9438b6e86d504b501a2e70
高江华 <g598670138@163.com>
2024-08-19 14:57:17 +0800

    完成悬浮球

--- components/global/g-suspend-ball/index.vue
+++ components/global/g-suspend-ball/index.vue
@@ -1,10 +1,13 @@
 <template>
 	<view>
 		<movable-area class="movable-area position-fixed bottom-0 left-0 right-0 w-100" :scale-area="false" :style="scrollStyle">
-			<movable-view class="movable-view d-flex a-center j-center rounded-circle" :class="!isRemove?'animation-info':''" style="pointer-events: auto;"
+			<movable-view class="movable-view position-relative d-flex a-center j-center rounded-circle" :class="!isRemove?'animation-info':''" style="pointer-events: auto;"
 				@click="clickBtn" @touchstart="touchstart" @touchend="touchend" @change="onChange" direction="all"
-				inertia="true" :x="x" :y="y" :disabled="disabled" :out-of-bounds="true" :damping="200" :friction="100">
+				inertia="true" :x="$store.state.ball.x" :y="$store.state.ball.y" :disabled="disabled" :out-of-bounds="true" :damping="200" :friction="100">
 				<slot></slot>
+				<view class="position-absolute bg-dark rounded-1 text-white left-half tf-half-x py px-2" style="bottom: -70rpx;">
+					{{$store.state.ball.x}}:{{$store.state.ball.y}}
+				</view>
 			</movable-view>
 		</movable-area>
 	</view>
@@ -41,8 +44,6 @@
 				isRemove: true,
 				btnWidth: 0,
 				btnHeight: 0,
-				x: 10000,
-				y: 10000,
 				old: {
 					x: 0,
 					y: 0
@@ -50,20 +51,41 @@
 			};
 		},
 		mounted() {
+			console.log(222)
 			this.getSysInfo()
 		},
 		methods: {
 			getSysInfo() {
-				// #ifndef H5
+				// #ifndef MP
 				uni.createSelectorQuery().in(this).select(".movable-view").boundingClientRect(rect => {
 					this.btnWidth = rect.width
 					this.btnHeight = rect.height
-					this.x = this.old.x
-					this.y = this.old.y
+					if (this.$store.state.ball.x == 0) {
 					this.$nextTick(res => {
-						this.x = this.$store.state.appSystemInfo.windowWidth - this.btnWidth - this.rightPx
-						this.y = this.$store.state.appSystemInfo.windowHeight - this.btnHeight - this.bottomPx
+						let newX = this.$store.state.appSystemInfo.windowWidth - this.btnWidth - this.rightPx
+						let newY = this.$store.state.appSystemInfo.windowHeight - this.btnHeight - (this.bottomPx * 2) - 
+						this.$store.state.browserFitHeight - this.$store.state.tabbarHeight - this.$store.state.statusHeight -
+						this.$store.state.navbarHeight - 100
+						uni.setStorageSync('ball', {x: newX, y: newY})
+						this.$store.commit('updateBall', {x: newX, y: newY})
 					})
+					}
+				}).exec() 
+				// #endif
+				// #ifdef MP
+				uni.createSelectorQuery().select(".movable-view").boundingClientRect(rect => {
+					this.btnWidth = rect.width
+					this.btnHeight = rect.height
+					if (this.$store.state.ball.x == 0) {
+					this.$nextTick(res => {
+						let newX = this.$store.state.appSystemInfo.windowWidth - this.btnWidth - this.rightPx
+						let newY = this.$store.state.appSystemInfo.windowHeight - this.btnHeight - (this.bottomPx * 2) -
+						this.$store.state.browserFitHeight - this.$store.state.tabbarHeight - this.$store.state.statusHeight -
+						this.$store.state.navbarHeight - 100
+						uni.setStorageSync('ball', {x: newX, y: newY})
+						this.$store.commit('updateBall', {x: newX, y: newY})
+					})
+					}
 				}).exec()
 				// #endif
 			},
@@ -79,16 +101,19 @@
 			//结束移动
 			touchend(e) {
 				if (this.canDocking && this.old.x) {
-					this.x = this.old.x
-					this.y = this.old.y
+					uni.setStorageSync('ball', {x: this.old.x, y: this.old.y})
+					this.$store.commit('updateBall', {x: this.old.x, y: this.old.y})
 					let bWidth = (this.$store.state.appSystemInfo.windowWidth - this.btnWidth) / 2
-					if (this.x < 0 || (this.x > 0 && this.x <= bWidth)) {
+					if (this.$store.state.ball.x < 0 || (this.$store.state.ball.x > 0 && this.$store.state.ball.x <= bWidth)) {
 						this.$nextTick(res => {
-							this.x = this.rightPx
+							uni.setStorageSync('ball', {x: this.rightPx, y: this.$store.state.ball.y})
+							this.$store.commit('updateBall', {x: this.rightPx, y: this.$store.state.ball.y})
 						})
 					} else {
 						this.$nextTick(res => {
-							this.x = this.$store.state.appSystemInfo.windowWidth - this.btnWidth - this.rightPx
+							let newX = this.$store.state.appSystemInfo.windowWidth - this.btnWidth - this.rightPx
+							uni.setStorageSync('ball', {x: newX, y: this.$store.state.ball.y})
+							this.$store.commit('updateBall', {x: newX, y: this.$store.state.ball.y})
 						})
 					}
 					this.isRemove = false
@@ -103,7 +128,7 @@
 			scrollStyle() {
 				return {
 					height: `calc(100vh - ${this.$store.state.browserFitHeight}px - ${this.rightPx * 2}px - ${this.$store.state.navbarHeight}px - ${this.$store.state.tabbarHeight}px - env(safe-area-inset-bottom) - ${this.$store.state.statusHeight}px)`,
-					top: `calc(${this.$store.state.statusHeight}px + ${this.$store.state.navbarHeight}px)`
+					top: `calc(${this.$store.state.statusHeight}px + ${this.$store.state.navbarHeight}px + ${this.rightPx}px)`
 				}
 			}
 		}
